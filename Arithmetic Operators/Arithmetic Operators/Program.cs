250939730e7ab02ebcb0d8c008446522ff663036
0reo27 <stopspammingme@hotmail.ph>
2018-07-12 22:41:49 +0800

    Arithmetic Operators

---
+++ Arithmetic Operators/Arithmetic Operators/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Employee ID
+            int employeeID;
+            Console.Write("Enter Employee ID: ");
+            employeeID = int.Parse(Console.ReadLine());
+
+            // Rate per day
+            int ratePerDay;
+            Console.Write("Enter Rate Per Day: ");
+            ratePerDay = int.Parse(Console.ReadLine());
+
+            // Number of days worked
+            int numberOfDaysWorked;
+            Console.Write("Enter Number Of Days Worked: ");
+            numberOfDaysWorked = int.Parse(Console.ReadLine());
+
+            // Basic Pay
+            int basicPay = ratePerDay * numberOfDaysWorked;
+            Console.WriteLine("The basic pay is: " + basicPay);
+
+            // Overtime
+            int overTime = 70;
+            Console.WriteLine("OT: " + overTime);
+
+            // Number of Overtime
+            int numberofOverTime;
+            Console.Write("Enter Number of Overtime: ");
+            numberofOverTime = int.Parse(Console.ReadLine());
+
+            // Gross Pay
+            int grossPay = basicPay + (overTime * numberofOverTime);
+            Console.Write("The Gross Pay is: " + grossPay);
+
+            // Pause
+            Console.ReadKey();
+
+        }
+    }
+}
